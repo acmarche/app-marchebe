d36b434051fe0f0a74beb697ca6ab8cf78c4dd4f
jfsenechal <jfsenechal@gmail.com>
2020-12-27 14:44:38 +0100

    renames routes

--- app/src/main/java/be/marche/www/MainActivity.kt
+++ app/src/main/java/be/marche/www/MainActivity.kt
@@ -3,30 +3,12 @@ package be.marche.www
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.setContent
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.navArgument
-import androidx.navigation.compose.rememberNavController
 import be.marche.www.event.EventViewModel
-import be.marche.www.event.ui.ListEventsComponent
-import be.marche.www.navigation.Actions
-import be.marche.www.navigation.Destinations.Agenda
-import be.marche.www.navigation.Destinations.EventShow
-import be.marche.www.navigation.Destinations.Home
-import be.marche.www.navigation.Destinations.News
-import be.marche.www.navigation.Destinations.NewsShow
-import be.marche.www.navigation.Destinations.TaskDetailArgs.newsId
+import be.marche.www.navigation.RegisterRoutes
 import be.marche.www.news.NewsViewModel
-import be.marche.www.news.ui.ListNewsComponent
-import be.marche.www.news.ui.NewsShowComponent
-import be.marche.www.ui.HomeComponent
 import be.marche.www.ui.MarcheComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -38,51 +20,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-
-            Launch(newsViewModel, eventViewModel)
-
-        }
-    }
-}
-
-@Composable
-fun Launch(newsViewModel: NewsViewModel, eventViewModel: EventViewModel) {
-    val navController = rememberNavController()
-    val actions = remember(navController) { Actions(navController) }
-
-    MarcheComposeTheme {
-        NavHost(navController = navController, startDestination = Home) {
-            composable(Home) {
-                HomeComponent(
-                    listNews = actions.listNews,
-                    listEvents = actions.listEvents
-                )
-            }
-            composable(News) {
-                ListNewsComponent(
-                    newsViewModel.allNews,
-                    onItemClick = actions.newsShow
-                )
-            }
-            composable(
-                "$NewsShow/{$newsId}",
-                arguments = listOf(navArgument(newsId) { type = NavType.IntType })
-            ) { backStackEntry ->
-                NewsShowComponent(
-                    newsId = backStackEntry.arguments?.getInt(newsId) ?: 0,
-                    //navigateUp = actions.navigateUp
-                )
-            }
-            composable(EventShow) {
-                NewsShowComponent(
-                    2
-                )
-            }
-            composable(Agenda) {
-                ListEventsComponent(
-                    eventViewModel.allEvents,
-                    onItemClick = actions.eventShow
-                )
+            MarcheComposeTheme {
+                RegisterRoutes(newsViewModel, eventViewModel)
             }
         }
     }
--- app/src/main/java/be/marche/www/navigation/NavGraph.kt
+++ app/src/main/java/be/marche/www/navigation/NavGraph.kt
@@ -1,41 +1,82 @@
 package be.marche.www.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.navigate
-import be.marche.www.navigation.Destinations.Agenda
-import be.marche.www.navigation.Destinations.EventShow
-import be.marche.www.navigation.Destinations.News
-import be.marche.www.navigation.Destinations.NewsShow
-import be.marche.www.navigation.Destinations.TaskDetailArgs.eventId
-import be.marche.www.navigation.Destinations.TaskDetailArgs.newsId
+import androidx.navigation.NavType
+import androidx.navigation.compose.*
+import be.marche.www.event.EventViewModel
+import be.marche.www.event.ui.ListEventsComponent
+import be.marche.www.news.NewsViewModel
+import be.marche.www.news.ui.ListNewsComponent
+import be.marche.www.news.ui.NewsShowComponent
+import be.marche.www.ui.HomeComponent
 
-object Destinations {
+object Routes {
     const val Home = "home"
     const val News = "listNews"
     const val Agenda = "listEvents"
-    const val EventShow = "eventShow"
-    const val NewsShow = "newsShow"
-
-    object TaskDetailArgs {
-        const val eventId = "eventId"
-        const val newsId = "newsId"
-    }
+    const val EventShow = "eventShow/{eventId}"
+    const val NewsShow = "newsShow/{newsId}"
 }
 
 class Actions(navController: NavHostController) {
     val listNews: () -> Unit = {
-        navController.navigate(News)
+        navController.navigate(Routes.News)
     }
-    val newsShow: (Int) -> Unit = { taskId ->
-        navController.navigate("$NewsShow/$taskId")
+    val newsShow: (Int) -> Unit = { newsId ->
+        navController.navigate("newsShow/$newsId")
     }
     val listEvents: () -> Unit = {
-        navController.navigate(Agenda)
+        navController.navigate(Routes.Agenda)
     }
-    val eventShow: (Int) -> Unit = { taskId ->
-        navController.navigate("$EventShow/$taskId")
+    val eventShow: (Int) -> Unit = { eventId ->
+        navController.navigate("eventShow/$eventId")
     }
     val navigateUp: () -> Unit = {
         navController.popBackStack()
+    }
+}
+
+@Composable
+fun RegisterRoutes(newsViewModel: NewsViewModel, eventViewModel: EventViewModel) {
+    val navController = rememberNavController()
+    val navigateTo = remember(navController) { Actions(navController) }
+
+    NavHost(navController = navController, startDestination = Routes.Home) {
+        composable(Routes.Home) {
+            HomeComponent(
+                listNews = navigateTo.listNews,
+                listEvents = navigateTo.listEvents
+            )
+        }
+        composable(Routes.News) {
+            ListNewsComponent(
+                newsViewModel.allNews,
+                onItemClick = navigateTo.newsShow
+            )
+        }
+        composable(
+            route = Routes.NewsShow,
+            arguments = listOf(navArgument("newsId") {
+                type = NavType.IntType
+            })
+        ) { backStackEntry ->
+            NewsShowComponent(
+                newsId = backStackEntry.arguments?.getInt("newsId") ?: 0,
+                //navigateUp = actions.navigateUp
+            )
+        }
+        composable(Routes.EventShow) {
+            NewsShowComponent(
+                2
+            )
+        }
+        composable(Routes.Agenda) {
+            ListEventsComponent(
+                eventViewModel.allEvents,
+                onItemClick = navigateTo.eventShow
+            )
+        }
     }
 }
