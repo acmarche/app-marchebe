b242bd87cb7156bcef3ed6f8a47b4828b8f814d3
jfsenechal <jfsenechal@gmail.com>
2020-12-28 23:24:15 +0100

    news detail

--- app/src/main/java/be/marche/www/news/ui/NewsContent.kt
+++ app/src/main/java/be/marche/www/news/ui/NewsContent.kt
@@ -1,21 +1,7 @@
-/*
- * Copyright 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package be.marche.www.news.ui
 
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.layout.*
@@ -26,14 +12,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Providers
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.AmbientContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
 import be.marche.www.model.News
 import be.marche.www.news.NewsViewModel
+import be.marche.www.ui.components.NetworkImageComponentPicasso
 
 private val defaultSpacerSize = 16.dp
 
@@ -42,27 +34,28 @@ private val defaultSpacerSize = 16.dp
 fun NewsShowComponent(newsId: Int, newsViewModel: NewsViewModel) {
 
     val news by newsViewModel.findById(newsId).observeAsState(initial = null)
-
-    Text("coucou id ${news?.intitule}")
+    news?.let { PostContent(it) }
 
 }
 
 @Composable
-fun PostContent(post: News, modifier: Modifier = Modifier) {
+fun PostContent(news: News) {
     ScrollableColumn(
-        modifier = modifier.padding(horizontal = defaultSpacerSize)
+        modifier = Modifier.padding(horizontal = defaultSpacerSize)
     ) {
         Spacer(Modifier.preferredHeight(defaultSpacerSize))
-        PostHeaderImage(post)
-        Text(text = post.intitule, style = MaterialTheme.typography.h4)
+        PostHeaderImage(news)
+        Text(text = news.intitule, style = MaterialTheme.typography.h4)
         Spacer(Modifier.preferredHeight(8.dp))
-        post.extrait?.let { subtitle ->
+
+        news.content?.let { content ->
             Providers(AmbientContentAlpha provides ContentAlpha.medium) {
-                Text(
-                    text = subtitle,
-                    style = MaterialTheme.typography.body2,
-                    lineHeight = 20.sp
-                )
+                PlantDescription(news.content)
+                /*     Text(
+                         text = PlantDescription(news.content),
+                         style = MaterialTheme.typography.body2,
+                         lineHeight = 20.sp
+                     )*/
             }
             Spacer(Modifier.preferredHeight(defaultSpacerSize))
         }
@@ -74,12 +67,17 @@ fun PostContent(post: News, modifier: Modifier = Modifier) {
 
 @Composable
 private fun PostHeaderImage(post: News) {
-    post.thumbnail?.let { image ->
+    post.image?.let { image ->
         val imageModifier = Modifier
             .heightIn(min = 180.dp)
             .fillMaxWidth()
             .clip(shape = MaterialTheme.shapes.medium)
-        // Image(image, imageModifier, contentScale = ContentScale.Crop)
+        //.scale(ContentScale.Crop)
+        NetworkImageComponentPicasso(
+            url = image,
+            modifier = imageModifier
+        )
+        //Image(image, imageModifier, contentScale = ContentScale.Crop)
         Spacer(Modifier.preferredHeight(defaultSpacerSize))
     }
 }
@@ -95,5 +93,57 @@ private fun PostMetadata(metadata: Metadata) {
             contentScale = ContentScale.Fit
         )
         Spacer(Modifier.preferredWidth(8.dp))
+    }
+}
+
+@Composable
+private fun rememberTextView(): TextView {
+    val context = AmbientContext.current
+    return remember {
+        TextView(context).apply {
+            movementMethod = LinkMovementMethod.getInstance()
+        }
+    }
+}
+
+@Composable
+private fun PlantDescription(description: String) {
+    val textView = rememberTextView()
+
+    val htmlDescription = remember(description) {
+        HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_COMPACT)
+    }
+
+    AndroidView({ textView }) {
+        it.text = htmlDescription
+    }
+
+    //essais mise en page
+    // style = MaterialTheme.typography.body2,
+    //  lineHeight = 20.sp
+    val modifier = Modifier.size(20.dp)
+
+    AndroidView(
+        //   modifier = Modifier.,
+        // The viewBlock provides us with the Context so we do not have to pass this down into the @Composable
+        // ourself
+        viewBlock = { context ->
+            // Inside the viewBlock we create a good ol' fashion TextView to match the width and height of its
+            // parent
+            //   textView.text = htmlDescription
+
+            TextView(context).apply {
+                text = htmlDescription
+            }
+        })
+
+
+}
+
+@Preview
+@Composable
+private fun PlantDescriptionPreview() {
+    MaterialTheme {
+        PlantDescription("HTML<br><br>description")
     }
 }
