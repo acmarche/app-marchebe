e9292d6afa944d918c25e47390e63ab5d6af9c5e
jfsenechal <jfsenechal@gmail.com>
2020-12-30 20:35:10 +0100

    add comment

--- app/src/main/java/be/marche/www/ui/components/Theme.kt
+++ app/src/main/java/be/marche/www/ui/components/Theme.kt
@@ -7,6 +7,23 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
+/**
+ * https://alexzh.com/jetpack-compose-theme-and-typography/
+ * Background colors
+ *  Primary color displayed most frequently across your app's screens and components;
+ *  Primary Variant color is used to distinguish elements using primary colors, such as top app bar and the system bar.
+ *  Secondary color provides more ways to accent and distinguish your product. Having a secondary color is optional, and should be applied sparingly to accent select parts of your UI;
+ *  Secondary Variant color is used to distinguish elements using secondary colours;
+ *  Background color appears behind scrollable content;
+ *  Surface color uses on surfaces of components, like cards and menus;
+ *  Error color used for indicating an error.
+ * Typography and icon colors
+ *  On Primary color of text and icons displayed on top of the primary color.
+ *  On Secondary color of text and icons displayed on top of the secondary color;
+ *  On Background color of text and icons displayed on top of the background color;
+ *  On Surface color of text and icons displayed on top of the surface color;
+ *  On Error color of text and icons displayed on top of the error color.
+ */
 private val DarkColorPalette = darkColors(
     primary = blue3,
     primaryVariant = blue1,
