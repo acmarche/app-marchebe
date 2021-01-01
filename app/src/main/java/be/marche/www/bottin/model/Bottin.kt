9a92013332c5a143137935d2ab218aacb8cb1a1f
jfsenechal <jfsenechal@gmail.com>
2021-01-01 22:57:26 +0100

    continue bottin

---
+++ app/src/main/java/be/marche/www/bottin/model/Bottin.kt
@@ -0,0 +1,16 @@
+package be.marche.www.bottin.model
+
+class Bottin {
+    companion object {
+        const val USER: String = "**"
+        const val PASSWORD: String = "**"
+        const val COMMERCES = 610
+        const val SANTE = 5
+        const val SERVICES_COMMUNAUX = 665
+        const val COLLEGE = 667
+        const val CONSEIL = 666
+        const val MEDECINS = 360
+        const val PHARMACIES = 390
+        const val LIBERALS = 591
+    }
+}
